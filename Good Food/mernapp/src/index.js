d17c112e468c8da55f539280588e01286e0e2f4b
Manisha <er.manisha08@gmail.com>
2023-10-16 17:09:56 +0530

    updated the server url

--- Good Food/mernapp/src/index.js
+++ Good Food/mernapp/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+var serverUrl = "https://goodfoodapp-server-82607cec3049.herokuapp.com/";
 root.render(
   <React.StrictMode>
     <App />
--- Good Food/mernapp/src/screens/MyOrder.js
+++ Good Food/mernapp/src/screens/MyOrder.js
@@ -7,17 +7,20 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/auth/myOrderData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: localStorage.getItem("userEmail"),
-      }),
-    }).then(async (res) => {
+    await fetch(
+      "https://goodfoodapp-server-82607cec3049.herokuapp.com/api/auth/myOrderData",
+      {
+        // credentials: 'include',
+        // Origin:"http://localhost:3000/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: localStorage.getItem("userEmail"),
+        }),
+      }
+    ).then(async (res) => {
       let response = await res.json();
       await setorderData(response);
     });
